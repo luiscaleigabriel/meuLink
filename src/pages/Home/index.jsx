d83f86c96fbaed0c3480510b2090901b995781d6
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-07-13 11:20:38 +0100

    Finalizei a home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,29 @@
+import { FiLink } from 'react-icons/fi'
+import './home.css'
+
+function Home() {
+  return (
+    <div className="container_home">
+      <div className="logo">
+        <img src="/principal.png" alt="LinkSnipper" />
+        <h1>LinkSnipper</h1>
+        <span>Cole seu link para encurtar 👇</span>
+      </div>
+      
+      <div className="are-input">
+        <label htmlFor='link'>
+          <FiLink size={24} color='#fff' />
+          <input 
+            type="text"
+            id='link'
+            placeholder='Cole seu link aqui!'
+          />
+        </label>
+
+        <button>Gerar Link</button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Links/index.jsx
@@ -0,0 +1,10 @@
+
+function Links() {
+  return (
+    <>
+      <h1>Li</h1>
+    </>
+  )
+}
+
+export default Links
