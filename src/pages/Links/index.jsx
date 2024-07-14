3a4fd4a9a562a0b27ad702af273a6e8dcb0f1d94
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-07-14 08:07:04 +0100

    Finalizei a home

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -1,9 +1,35 @@
+import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
+import './links.css'
+import { Link } from 'react-router-dom'
 
 function Links() {
   return (
-    <>
-      <h1>Li</h1>
-    </>
+    <div className="links_contain">
+      <div className="links_header">
+        <Link to='/'>
+          <FiArrowLeft size={38} color='#fff' />
+        </Link>
+        <h1>Meus Links</h1>
+      </div>
+      <div className="links_item">
+        <button className='link'>
+          <FiLink size={18} color='#fff' />
+          https://github.com/luiscaleigabriel/loja_LGPJ_com_php_puro
+        </button>
+        <button className='link_delete'>
+          <FiTrash size={18} color='#ff5454' />
+        </button>
+      </div>
+      <div className="links_item">
+        <button className='link'>
+          <FiLink size={18} color='#fff' />
+          https://github.com/luiscaleigabriel/loja_LGPJ_com_php_puro
+        </button>
+        <button className='link_delete'>
+          <FiTrash size={18} color='#ff5454' />
+        </button>
+      </div>
+    </div>
   )
 }
 
